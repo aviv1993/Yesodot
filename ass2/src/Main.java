e143738c4ec5d33177691041d879c639a963c9b0
Aviv Milnik <milnik@post.bgu.ac.il>
2020-01-14 19:02:23 +0200

    Prep

--- ass2/src/Main.java
+++ ass2/src/Main.java
@@ -10,24 +10,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
-        ProjectManagment pm = new ProjectManager(new UserDbImplement(),new ProjectDbImplement(),new DepartmentPrinter("CS"));
-        pm.register("avi","avi",false);
-        pm.login("avi","avi");
-        List<String> restricted = new ArrayList<>();
-        restricted.add("meme");
-        int code = pm.addProject("avi","asd","asd","201","asd","asd","asd","asd","asd");
-
-        pm.addBasicWebsiteToProject(code,"BASE TO THE BASE");
-        pm.restrictUsers(restricted,code);
-        pm.decorateSite("design1","LOGOGOGOGOGOGOGOGG",code);
-        pm.decorateSite("design2","menu 1 2 34",code);
-        pm.decorateSite("design3","fafa.mp3",code);
-        pm.decorateSite("design4","https://",code);
-        pm.decorateSite("logo","DESIGN 5",code);
-        pm.restrictUsers(restricted,code);
-
-        System.out.println(pm.getWebsite("meme",code).buildSite());
-        //pm;
     }
 
 }
